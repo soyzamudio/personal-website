e31f68321c86fff8d7a76ccd59324921a3bc5809
Jose Zamudio <zamudio@outlook.com>
2015-02-08 14:38:07 -0800

    Add smooth scroll to sections

--- js/fuckitJquery.js
+++ js/fuckitJquery.js
@@ -7,6 +7,31 @@ $(document).ready(function() {
       $('.navbar').fadeOut({duration: 800});
     }
   });
+
+  $(".intro-scroll").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#intro").offset().top}, 'slow');
+  });
+
+  $(".portfolio-scroll").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#portfolio").offset().top}, 'slow');
+  });
+
+  $(".experience-scroll").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#experience").offset().top}, 'slow');
+  });
+
+  $(".blog-scroll").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#blog").offset().top}, 'slow');
+  });
+
   $('#intro').parallax({imageSrc: './img/bg.jpg', speed: 0.4, position: '-50', bleed: 30});
   $('.intro-items').addClass('animated fadeIn');
   $(".navbar").hide();
