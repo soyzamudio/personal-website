9e6c7da066e55d6c5d453e76f6c2d7eae4f3c87e
Jose Zamudio <zamudio@outlook.com>
2015-02-08 18:59:52 -0800

    Add contact form modal

--- js/fuckitJquery.js
+++ js/fuckitJquery.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  $(".navbar").hide();
   $(window).scroll(function () {
     if ($(this).scrollTop() > 600) {
       $('.navbar').fadeIn({duration: 800});
@@ -34,6 +35,55 @@ $(document).ready(function() {
 
   $('#intro').parallax({imageSrc: './img/bg.jpg', speed: 0.4, position: '-50', bleed: 30});
   $('.intro-items').addClass('animated fadeIn');
-  $(".navbar").hide();
+
+  $(".form").on('click', function(e) {
+    e.preventDefault()
+    $('#myModal').modal();
+  });
+
+  $(".sendForm").on('click', function() {
+    var email = {key: "DV6iDBf0ZF_bcscqfJalXw",
+                 message: {from_email: $('#inputEmail').val(),
+                           to: 'All'[{email: "jose@josezamudio.me",
+                                 type: "to" }],
+                           autotext: "true",
+                           html: '<div>Name: ' + $('#inputName').val() + '</div><div>Website: ' + $('#inputWebsite').val() + '</div><div>Message: ' + $('#inputMessage').val() + '</div>'}
+                };
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: email,
+      success: function(msg){
+        $('#inputName').val('');
+        $('#inputEmail').val('');
+        $('#inputWebsite').val('');
+        $('#inputMessage').val('');
+        $('.alert').removeClass('hidden');
+        $('.alert').addClass('alert-success');
+        $('.alert').html('<strong>Success</strong> Email sent!');
+      }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+        $('.alert').removeClass('hidden');
+        $('.alert').addClass('alert-danger');
+        $('.alert').html('<strong>Success</strong> Email sent!');
+      }
+    });
+  });
+
+//  $(".sendForm").on('click', function(e) {
+//    $('#inputEmail').val,
+////    $.ajax({
+////      type: “POST”,
+////      url: ,
+////      data:  })
+////    .done(function(response) {
+////      console.log(response); // if you're into that sorta thing
+////    });
+////
+////    $.post("https://mandrillapp.com/api/1.0/messages/send.json'", function(data, status){
+////      alert("Data: " + data + "\nStatus: " + status);
+////    });
+//  })
 
 });
+
+
