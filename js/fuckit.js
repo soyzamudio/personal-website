95b9375aa1e5c67ed70269ba41c18e969ca5b3fa
Jose Zamudio <zamudio@outlook.com>
2015-02-08 23:14:20 -0800

    Fix the wrong AirTym website...

--- js/fuckit.js
+++ js/fuckit.js
@@ -4,7 +4,7 @@ jz.controller("ZamudioController", ['$scope', function($scope) {
 
   $scope.portfolio = [
     { title: "CarWise", category: "iOS", website: "http://www.carwise.me", image: "img/carwise-app.jpg" },
-    { title: "AirTym", category: "iOS, Web", website: "http://www.airtym.me", image: "img/airtym-app.jpg" },
+    { title: "AirTym", category: "iOS, Web", website: "http://www.airtym.co", image: "img/airtym-app.jpg" },
     { title: "TweeterBoard", category: "Web", website: "http://tweeterboard.herokuapp.com/", image: "img/tweeterboard-app.jpg" }
   ];
 
