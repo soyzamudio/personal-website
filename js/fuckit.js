f31ced95fa35bce5434a733d444d8f4cb3887482
Jose Zamudio <zamudio@outlook.com>
2015-02-12 11:29:38 -0800

    Add new Weather app project

--- js/fuckit.js
+++ js/fuckit.js
@@ -14,7 +14,8 @@ jz.controller("ZamudioController", ['$scope', function($scope) {
     { title: "CarWise", category: "iOS", website: "http://www.carwise.me", image: "img/carwise-app.jpg" },
     { title: "AirTym", category: "iOS, Web", website: "http://www.airtym.co", image: "img/airtym-app.jpg" },
     { title: "TweeterBoard", category: "Web", website: "http://tweeterboard.herokuapp.com/", image: "img/tweeterboard-app.jpg" },
-    { title: "iOS Calculator", category: "Web", website: "https://github.com/soyzamudio/iOS-Calculator-Javascript/", image: "" }
+    { title: "iOS Calculator", category: "Web", website: "https://soyzamudio.github.io/iOS-Calculator-Javascript/", image: "" },
+    { title: 'Weather App', category: 'Web', website: 'http://soyzamudio.github.io/weather-app/', image: ''}
   ];
 
   $scope.experience = [
