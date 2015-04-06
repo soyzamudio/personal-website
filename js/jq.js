52a89a9c70ea0fee27d0c842f79f62f64761f758
Jose Zamudio <zamudio@outlook.com>
2015-04-06 00:09:33 -0700

    Fix tick issue

--- js/jq.js
+++ js/jq.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 8) || 2000;
+    this.period = parseInt(period, 10) || 2000;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
